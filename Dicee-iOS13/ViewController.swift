5d8b89c5b0202597fe6769cfc72049732a7476e9
Brianna Tran <briannatran45@gmail.com>
2023-12-21 14:59:32 -0800

    made new connection

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -10,14 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // IBOutlet allows me to reference a UI elem
     @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
+        // WHO         WHAT    VALUE
         diceImageView1.image = UIImage(named: "DiceSix")
-        diceImageView1.alpha = 0.5
+//        diceImageView1.alpha = 0.5
+        diceImageView2.image = UIImage(named: "DiceTwo")
     }
 
 
