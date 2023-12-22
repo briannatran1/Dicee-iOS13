84b73d96db38c9e00cd12967a214e04cf51e3a46
Brianna Tran <briannatran45@gmail.com>
2023-12-21 16:16:34 -0800

    added randomization to dice

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -15,27 +15,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-        
-    }
-    
     // IBAction: interaction with UI leads to something happening in our code
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"),
-                                UIImage(named: "DiceThree"), UIImage(named: "DiceFour"),
-                                UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][leftDiceNumber]
         
-        diceImageView2.image = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"),
-                                UIImage(named: "DiceThree"), UIImage(named: "DiceFour"),
-                                UIImage(named: "DiceFive"), UIImage(named: "DiceSix")][rightDiceNumber]
+        var diceArray = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"),
+                         UIImage(named: "DiceThree"), UIImage(named: "DiceFour"),
+                         UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
         
-        leftDiceNumber = leftDiceNumber + 1
-        rightDiceNumber = rightDiceNumber - 1
+        diceImageView1.image = diceArray[Int.random(in: 0...5)] // ... specifies a range
+        diceImageView2.image = diceArray[Int.random(in: 0...5)]
     }
     
 }
